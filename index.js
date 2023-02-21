f9a040c3c6810f18100320259acdeb236a57caa8
Danil <danil.kersontsev@khk.ee>
2023-02-21 10:51:21 +0200

    Created connection to database

--- index.js
+++ index.js
@@ -4,10 +4,25 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 
+// connect to database
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize')
+
+// testing connection
+sequelize
+    .authenticate()
+    .then(() =>{
+        console.log('Connected to the database');
+    })
+    .catch(err=> {
+        console.error('Unable to connect to the database:', err)
+    });
+
+// simple route
 app.get ("/", (req,res) =>{
     res.json({ message: "Welcome to sequelize application."});
 });
-
+// listen requests
 app.listen(3000, () =>{
     console.log("Server is running on http://localhost:3000")
 })
