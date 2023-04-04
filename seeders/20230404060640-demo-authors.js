4450696434a4112a037e532d9317977e138ae1b5
Danil <danil.kersontsev@khk.ee>
2023-04-04 09:44:50 +0300

    Created author table and added table data

---
+++ seeders/20230404060640-demo-authors.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up (queryInterface, Sequelize) {
+    return Promise.all([
+      queryInterface.bulkInsert('Authors', [{
+        name:'Ashley Galvin',
+        author_id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }]),
+      queryInterface.bulkInsert('Authors', [{
+        name:'Patrick Beach',
+        author_id: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }]),
+      queryInterface.bulkInsert('Authors', [{
+        name:'MacKenzie Miller',
+        author_id: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }]),
+    ])
+  },
+
+  down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Authors', null, {});
+  }
+};
